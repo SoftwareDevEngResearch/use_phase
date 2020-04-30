d1b64738a059bd7b6bc405c72f272628dd17eaad
Melissa <63125084+tensam1@users.noreply.github.com>
2020-04-30 10:05:51 -0700

    create user_input.py

---
+++ userinput.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 16:55:39 2020
+
+@author: melis
+"""
+#getting lower limits
+product_name=input("Please enter the name of the product: "/n)
+product_min_use_amount_day=input('Please enter the minimum amount of time a user would use this product each time it is used: '/n)
+product_min_use_amount_week=input('Please enter the minimum amount of time a user would use this product a week, ignore if you use it less than once a week'/n)
+product_min_use_amount_month=input('If your product is used less than once a week, enter the minimum number of times a year it is used)
+
+#getting average
+product_avg_use_amount_day=input('Please enter the average amount of time a user would use this product each time it is used: '/n)
+product_avg_use_amount_week=input('Please enter the average amount of time a user would use this product a week, ignore if you use it less than once a week'/n)
+product_avg_use_amount_month=input('If your product is used less than once a week, enter the average number of times a year it is used)
+
+#getting upper limits
+product_max_use_amount_day=input('Please enter the maximum amount of time a user would use this product each time it is used: '/n)
+product_max_use_amount_week=input('Please enter the maximum amount of time a user would use this product a week, ignore if you use it less than once a week'/n)
+product_max_use_amount_month=input('If your product is used less than once a week, enter the maximum number of times a year it is used)
+
